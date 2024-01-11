2636bc38c164f44407fcfd9373f146368aa2f9f2
thyagopereira <thyago@DESKTOP-E7A46HB>
2024-01-11 18:53:33 -0300

    Database conn

--- desafioNode/node/index.js
+++ desafioNode/node/index.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
-// const config = {
-//     host: 'db',
-//     user: 'root',
-//     password: 'root',
-//     database:'nodedb'
-// };
-// const mysql = require('mysql')
-// const connection = mysql.createConnection(config)
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'fullcycleDB',
+    database:'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
 
-// const sql = `INSERT INTO people(name) values('Wesley')`
-// connection.query(sql)
-// connection.end()
+const sql = `INSERT INTO people(name) values('Wesley')`
+connection.query(sql)
+connection.end()
 
 
 app.get('/', (req,res) => {
