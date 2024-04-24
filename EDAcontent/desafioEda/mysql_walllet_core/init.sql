20ef00e636bc82d3a90cc188782a546e9b4bed8b
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2024-04-24 20:22:00 -0300

    Adding up mysql walletCore config

---
+++ EDAcontent/desafioEda/mysql_walllet_core/init.sql
@@ -0,0 +1,7 @@
+-- Add here commands to init Wallet Core database with
+
+-- CREATE TABLE test (
+--     id int NOT NULL AUTO_INCREMENT,
+--     nome VARCHAR(50) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
